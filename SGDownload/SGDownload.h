7435d7ebc12f6509b692310228eb8472fcb9e504
Single <musicman_leehom@126.com>
2017-03-17 14:43:28 +0800

    Initial Commit

---
+++ SGDownload/SGDownload.h
@@ -0,0 +1,19 @@
+//
+//  SGDownload.h
+//  SGDownload
+//
+//  Created by Single on 2017/3/17.
+//  Copyright © 2017年 single. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SGDownload.
+FOUNDATION_EXPORT double SGDownloadVersionNumber;
+
+//! Project version string for SGDownload.
+FOUNDATION_EXPORT const unsigned char SGDownloadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SGDownload/PublicHeader.h>
+
+
