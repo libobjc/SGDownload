d0789d502e51c4aa7207de155f7a24255986480c
Single <musicman_leehom@126.com>
2017-03-23 17:18:09 +0800

    add demo

---
+++ demo/demo-ios/demo-ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo-ios
+//
+//  Created by Single on 2017/3/23.
+//  Copyright © 2017年 Single. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demo/demo-macos/demo-macos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo-macos
+//
+//  Created by Single on 2017/3/23.
+//  Copyright © 2017年 Single. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
